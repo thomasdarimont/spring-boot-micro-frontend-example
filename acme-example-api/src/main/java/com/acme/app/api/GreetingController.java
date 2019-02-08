95200d0f78093646ef9b635135257e3cb7c9524b
Thomas Darimont <thomas.darimont@googlemail.com>
2019-02-08 23:38:44 +0100

    Revise demo
    Signed-off-by: Thomas Darimont <thomas.darimont@googlemail.com>

--- acme-example-api/src/main/java/com/acme/app/api/GreetingController.java
+++ acme-example-api/src/main/java/com/acme/app/api/GreetingController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/greetings")
 class GreetingController {
 
 	@GetMapping
 	Object greet(@RequestParam(defaultValue = "world") String name) {
-		return Map.of("greeting", "Hello " + name, "time", System.currentTimeMillis());
+		Map<String, Object> data = Map.of("greeting", "Hello " + name, "time", System.currentTimeMillis());
+		log.info("\nReturning: {}", data);
+		return data;
 	}
 }
